5a1f2d00843f22071de54a08ebe696ac61db3c56
hisomura <hisomura.noreply@gmail.com>
2020-11-29 20:34:47 +0900

    Implement gapi authentication.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import "./App.css";
 import { initGapiClient } from "./lib/gapi";
 
+const signInClickHandler: MouseEventHandler<HTMLButtonElement> = (_event) => {
+  gapi.auth2.getAuthInstance().signIn();
+};
+
+const signOutClickHandler: MouseEventHandler<HTMLButtonElement> = (_event) => {
+  gapi.auth2.getAuthInstance().signOut();
+};
+
 export default function App() {
   const [isInitialized, setIsInitialized] = useState(false);
+  const [isSignedIn, setSignedIn] = useState(false);
 
   useEffect(() => {
-    const init = () => initGapiClient().then(() => setIsInitialized(true));
+    const init = () =>
+      initGapiClient().then(() => {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(setSignedIn);
+        setIsInitialized(true);
+        setSignedIn(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
     if (gapi !== undefined) {
       init();
     } else {
@@ -20,10 +34,22 @@ export default function App() {
     return <div>waiting...</div>;
   }
 
+  if (!isSignedIn) {
+    return (
+      <div>
+        <button type="button" onClick={signInClickHandler}>
+          Sign in.
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="text-red-100">hello</div>
-      <p>Allow this application to get your Google Tasks data.</p>
+      <button type="button" onClick={signOutClickHandler}>
+        Sign out.
+      </button>
     </div>
   );
 }
