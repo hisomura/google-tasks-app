69ab96c4c42007cc936989a6f248c10db0a18f04
hisomura <hisomura.noreply@gmail.com>
2020-12-16 07:54:12 +0900

    Fix some tests by injecting redux store.

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,14 +1,24 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { render, RenderOptions } from "@testing-library/react";
+import { render as rtlRender, RenderOptions } from "@testing-library/react";
 import { FC, ReactElement } from "react";
+import { Provider } from "react-redux";
+import { rootReducer } from "./store/store";
+import { createStore, Store } from "redux";
 
-const AllTheProviders: FC = ({ children }) => {
-  return <>{children}</>;
-};
+interface Options extends RenderOptions {
+  initialState?: {};
+  store?: Store;
+}
 
-const customRender = (ui: ReactElement, options?: RenderOptions) => {
-  render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options: Options = {}) => {
+  const { initialState, store, ...renderOptions } = options;
+  const testStore = store ?? createStore(rootReducer, initialState);
+  const Wrapper: FC = (props) => {
+    return <Provider store={testStore}>{props.children}</Provider>;
+  };
+
+  rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 };
 
 // re-export everything
